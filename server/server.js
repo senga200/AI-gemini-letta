3a6ee6f42cfa64af60519dee3d5e1f13c943f71f
agnes <agnes.capello@magmagroup.fr>
2025-06-26 12:40:20 +0200

    ajout path fichier statique

--- server/server.js
+++ server/server.js
@@ -2,12 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir le dossier public statique
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //////////////GEMINI API////////////////////
 

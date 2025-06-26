a977f00df85a1dcc7ec69d7fbbc1b857b7a58eed
agnes <agnes.capello@magmagroup.fr>
2025-06-26 12:48:25 +0200

    fix path

--- server/server.js
+++ server/server.js
@@ -16,7 +16,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Servir le dossier public statique
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 
 //////////////GEMINI API////////////////////
